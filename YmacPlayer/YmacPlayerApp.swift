e4202bafc66810d679980c3d471e12060b702a7a
vbazavliuk <valentin.bazavliuk@gmail.com>
2026-07-30 11:27:46 +0200

    Initial Commit

---
+++ YmacPlayer/YmacPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YmacPlayerApp.swift
+//  YmacPlayer
+//
+//  Created by Valentyn Bazavluk on 30.07.26.
+//
+
+import SwiftUI
+
+@main
+struct YmacPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
